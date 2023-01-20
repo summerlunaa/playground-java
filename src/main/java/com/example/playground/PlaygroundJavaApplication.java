32e66532c4e93fcfe6f6f9d8c31566950e9eaed8
summerluna <summerluna@summerlunaui-MacBookPro.local>
2023-01-20 14:39:16 +0900

    feat: initialize project

---
+++ src/main/java/com/example/playground/PlaygroundJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/playground/PlaygroundJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
